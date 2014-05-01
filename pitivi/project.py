cf8674c88d77092746cb0f9c25af562531d5ade4
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2014-05-01 16:56:05 +0200

    scenarios: adding asset requires an id, not an uri
    https://bugzilla.gnome.org/show_bug.cgi?id=739251

--- pitivi/project.py
+++ pitivi/project.py
@@ -75,7 +75,7 @@ class AssetRemovedAction(UndoableAction):
 
     def serializeLastAction(self):
         st = Gst.Structure.new_empty("remove-asset")
-        st.set_value("uri", self.asset.get_info().get_uri())
+        st.set_value("id", self.asset.get_info().get_uri())
         type_string = GObject.type_name(self.asset.get_extractable_type())
         st.set_value("type", type_string)
         return st.to_string()
@@ -95,7 +95,7 @@ class AssetAddedAction(UndoableAction):
 
     def serializeLastAction(self):
         st = Gst.Structure.new_empty("add-asset")
-        st.set_value("uri", self.asset.get_info().get_uri())
+        st.set_value("id", self.asset.get_info().get_uri())
         type_string = GObject.type_name(self.asset.get_extractable_type())
         st.set_value("type", type_string)
         return st.to_string()
